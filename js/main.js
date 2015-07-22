e6cbaaeb7b60a1a0dcba4643c0303908cb238494
Oleg <evo9.81@gmail.com>
2015-07-22 16:49:03 +0300

    change

--- js/main.js
+++ js/main.js
@@ -23,36 +23,41 @@
         d3.json('http://50.116.39.186/get_api_data.php', function(error, data) {
             if (data && data !== 'undefined') {
                 var offenses = data.response.result.rows;
-
-                var pins = [];
-                for(var i = 0; i < offenses.length; i ++) {
-                    var offense = offenses[i];
-                    if (offense.length > 0) {
-                        var title = offense[0];
-                        var coordinates = offense[1];
-                        if (coordinates.length > 0) {
-                            coordinates = coordinates.split(',');
-                            var pin = {
-                                latitude: coordinates[0],
-                                longitude: coordinates[1],
-                                radius: 5,
-                                borderWidth: 0,
-                                title: title
-                            }
-                            pins.push(pin);
-                        }
-                    }
-                }
-                map.pins(pins, {
-                    popupTemplate: function (geo, data) {
-                        return data.title;
-                    }
-                });
+                var i = 0;
+                drawPin(offenses, i);
             }
         });
     }
 
     function redraw() {
         map.svg.selectAll("g").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+
+    function drawPin(offenses, i) {
+        if(i < offenses.length) {
+            var offense = offenses[i];
+            if (offense.length > 0) {
+                var title = offense[0];
+                var coordinates = offense[1];
+                if (coordinates.length > 0) {
+                    coordinates = coordinates.split(',');
+                }
+                map.pins({
+                    latitude: coordinates[0],
+                    longitude: coordinates[1],
+                    radius: 5,
+                    borderWidth: 0
+                }, {
+                    popupTemplate: function (geo, data) {
+                        return data.title;
+                    }
+                });
+                setTimeout(function() {
+                    i ++;
+                    drawPin(offenses, i);
+                }, 5000);
+            }
+        }
+
     }
 })();
